0d8b622a44fcfaceec651f9633b16faf2979d8c3
Radosław Brzeziński <radekmac@Mac.lan>
2024-12-06 22:41:10 +0100

    fix deployer

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './components/Home';
-import PrivateRoute from './components/PrivateRoute';
 
 function App() {
   return (
