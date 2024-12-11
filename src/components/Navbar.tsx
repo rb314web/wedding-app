73d47046604c7cf4170fe20bf4957c0c126eb225
Radosław Brzeziński <radekmac@Mac.lan>
2024-12-12 00:06:45 +0100

    test

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -6,9 +6,9 @@ function Navbar() {
     <nav className='navbar'>
         <div className='navbar_logo'>LOGO</div>
       <ul className='navbar_ul'>
-        <li><a href="#">Strona główna</a></li>
-        <li><a href="#">O nas</a></li>
-        <li><a href="#">Kontakt</a></li>
+        <li><a href="/">Strona główna</a></li>
+        <li><a href="/">O nas</a></li>
+        <li><a href="/">Kontakt</a></li>
       </ul>
     </nav>
   );
