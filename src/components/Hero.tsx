b6954691f58014a9affedf08dccf57a5b888316d
Radosław Brzeziński <radekmac@Mac.lan>
2024-12-12 00:04:01 +0100

    test

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -6,7 +6,7 @@ const HeroSection = () => {
   return (
     <section className="hero">
       <div className="hero_content">
-        <h1 className="hero_content_title"></h1>
+        {/* <h1 className="hero_content_title"></h1> */}
         <p className="hero_content_subtitle">Efektywniej. Wydajniej. Razem.</p>
         <button className="hero_content_button">Sprawdz naszą ofertę</button>
       </div>
